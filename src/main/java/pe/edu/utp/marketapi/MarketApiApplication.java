65a401d6194a717666dc8174f8ed39cad5725e61
Junior-SB <subcerdo33@gmail.com>
2024-11-25 18:05:22 -0500

    Update MarketApiApplication.java
    correction cors

--- src/main/java/pe/edu/utp/marketapi/MarketApiApplication.java
+++ src/main/java/pe/edu/utp/marketapi/MarketApiApplication.java
@@ -2,6 +2,10 @@ package pe.edu.utp.marketapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MarketApiApplication {
@@ -9,5 +13,19 @@ public class MarketApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketApiApplication.class, args);
     }
+
+    @Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 
 }
